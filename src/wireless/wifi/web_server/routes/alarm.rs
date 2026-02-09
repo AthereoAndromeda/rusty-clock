e84af9fc5977eab44b330e44024a3e1503060451
Athereo <athereoandromeda@gmail.com>
2026-02-09 20:07:40 +0800

    feat: Remove `jiff` crate

--- src/wireless/wifi/web_server/routes/alarm.rs
+++ src/wireless/wifi/web_server/routes/alarm.rs
@@ -1,3 +1,4 @@
+use chrono::Timelike;
 use ds3231::Alarm1Config;
 use picoserve::{
     Router,
@@ -39,12 +40,14 @@ async fn get_alarm() -> impl IntoResponse {
 }
 
 async fn set_alarm_inner(hour: u8, min: u8, sec: u8, is_utc: bool) {
-    let base_time = jiff::civil::time(hour as i8, min as i8, sec as i8, 0);
+    let base_time = chrono::NaiveTime::from_hms_opt(hour as u32, min as u32, sec as u32).unwrap();
 
     let time = if is_utc {
         base_time
     } else {
-        base_time.wrapping_sub(jiff::Span::new().hours(TZ_OFFSET))
+        base_time
+            .overflowing_sub_signed(chrono::TimeDelta::hours(TZ_OFFSET as i64))
+            .0
     };
 
     #[cfg(debug_assertions)]
