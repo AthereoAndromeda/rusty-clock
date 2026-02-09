84219ce890b49ee0d7d94c2e9412b09c7fd18408
Athereo <athereoandromeda@gmail.com>
2026-02-09 19:20:15 +0800

    feat: Add stream for buzzer

--- src/wireless/wifi/web_server/routes/buzzer.rs
+++ src/wireless/wifi/web_server/routes/buzzer.rs
@@ -1,9 +1,27 @@
 use crate::buzzer::{BUZZER_SIGNAL, BuzzerAction, IS_BUZZER_ON};
+use embassy_time::Timer;
 use picoserve::{
     Router,
     response::{DebugValue, IntoResponse},
     routing::{PathRouter, get},
 };
+
+struct BuzzerEvent;
+
+impl picoserve::response::sse::EventSource for BuzzerEvent {
+    async fn write_events<W: picoserve::io::Write>(
+        self,
+        mut writer: picoserve::response::sse::EventWriter<'_, W>,
+    ) -> Result<(), W::Error> {
+        loop {
+            let a = IS_BUZZER_ON.load(core::sync::atomic::Ordering::SeqCst);
+            let res = if a { "true" } else { "false" };
+
+            writer.write_event("", res).await?;
+            Timer::after_secs(1).await;
+        }
+    }
+}
 
 pub fn add_routes(router: Router<impl PathRouter>) -> Router<impl PathRouter> {
     router
@@ -11,6 +29,10 @@ pub fn add_routes(router: Router<impl PathRouter>) -> Router<impl PathRouter> {
         .route("/buzzer/toggle", get(toggle_buzzer))
         .route("/buzzer/on", get(toggle_buzzer_on))
         .route("/buzzer/off", get(toggle_buzzer_off))
+        .route(
+            "/buzzer/stream",
+            get(async || picoserve::response::EventStream(BuzzerEvent)),
+        )
 }
 
 async fn get_buzzer() -> impl IntoResponse {
