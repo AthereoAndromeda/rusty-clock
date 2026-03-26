c403bd6f5b68efc2a4497b00633fb64d24c5c241
Athereo <athereoandromeda@gmail.com>
2026-03-26 19:14:20 +0800

    feat(web): Add display route for LCD

--- src/wireless/wifi/web_server/routes/lcd.rs
+++ src/wireless/wifi/web_server/routes/lcd.rs
@@ -1,16 +1,35 @@
 use picoserve::{
     Router,
+    extract::Form,
     response::IntoResponse,
     routing::{PathRouter, get},
 };
+use serde::Deserialize;
 
-use crate::lcd::{LCD_COMMANDS, LcdAction};
+use crate::lcd::{LCD_COMMANDS, LcdAction, LcdDisplayString};
 
 #[inline]
 pub(super) fn add_routes(router: Router<impl PathRouter>) -> Router<impl PathRouter> {
     let router = router.route("/lcd/on", get(backlight_control_on));
     let router = router.route("/lcd/off", get(backlight_control_off));
+    let router = router.route("/lcd/display", get(lcd_display));
+
     router.route("/lcd/toggle", get(backlight_control_toggle))
+}
+
+#[derive(Deserialize)]
+struct DisplayForm {
+    s1: LcdDisplayString,
+    s2: Option<LcdDisplayString>,
+}
+
+#[inline]
+async fn lcd_display(Form(form): Form<DisplayForm>) {
+    if let Some(s2) = form.s2 {
+        LCD_COMMANDS.signal(LcdAction::DisplayLines(form.s1, s2));
+    } else {
+        LCD_COMMANDS.signal(LcdAction::Display(form.s1));
+    }
 }
 
 #[inline]
